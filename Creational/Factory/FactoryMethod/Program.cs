1cf98acbdc60aadbabab18093dbe1b7de37d9c9a
Serhii Prostakov <s.prostakov@yandex.com>
2017-12-24 18:42:14 +0200

    Implemented factory method

--- Creational/Factory/FactoryMethod/Program.cs
+++ Creational/Factory/FactoryMethod/Program.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using static System.Console;
 
 namespace FactoryMethod
 {
+    public enum CoordinateSystem
+    {
+        Cartesian, Polar
+    }
+
+    public class Point
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}; {nameof(y)}: {y}";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            WriteLine(point);
         }
     }
 }

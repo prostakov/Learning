27f02032a0d149aeb474eb431289a69aa389dc77
Serhii Prostakov <s.prostakov@yandex.com>
2017-12-25 15:42:17 +0200

    Completed Prototype>Serialization

---
+++ Creational/Prototype/DeepCopy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace DeepCopy
+{
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+
+    public class Person : IPrototype<Person>
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+
+        public Person(string name, Address address)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(Name, Address.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    public class Address : IPrototype<Address>
+    {
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public Address DeepCopy()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person("John Smith", new Address("Petrykiv", 5));
+            Console.WriteLine(john);
+
+            var jane = john.DeepCopy();
+            jane.Name = "Jane Smith";
+            Console.WriteLine(jane);
+        }
+    }
+}
---
+++ Creational/Prototype/Serialization/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    public static class ExtensionMethods
+    {
+        public static T DeepCopy<T>(this T self)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, self);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T) formatter.Deserialize(stream);
+            }
+        }
+
+        public static T DeepCopyXml<T>(this T self)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stream, self);
+                stream.Position = 0;
+                return (T) serializer.Deserialize(stream);
+            }
+        }
+
+        public static T DeepCopyJson<T>(this T self)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Person));
+                serializer.WriteObject(stream, self);
+                stream.Position = 0;
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+
+        // TODO: NewtonsoftJsonSerializaion implementation
+    }
+    
+    //[Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+
+        public Person() { }
+
+        public Person(string name, Address address)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    //[Serializable]
+    public class Address
+    {
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+
+        public Address() {}
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person("John Smith", new Address("Petrykiv", 5));
+            Console.WriteLine(john);
+
+            var jane = john.DeepCopyJson();
+            jane.Name = "Jane Smith";
+            jane.Address.StreetName = "Berdychiv";
+            Console.WriteLine(jane);
+        }
+    }
+}

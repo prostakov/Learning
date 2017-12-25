6c30cb52233b601a07eb0b2c4736231f615e0d0e
Serhii Prostakov <s.prostakov@yandex.com>
2017-12-26 01:14:52 +0200

    Completed Monostate

---
+++ Creational/Singleton/Monostate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Monostate
+{
+    public class CEO
+    {
+        private static string _name;
+        private static int _age;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo1 = new CEO();
+            ceo1.Name = "Adam Smith";
+            ceo1.Age = 55;
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
+        }
+    }
+}

6df95ef8b8795ae44ee11b6e7908453efe69bed3
Serhii Prostakov <serhii.prostrakov@gmail.com>
2021-11-01 22:13:52 +0200

    Add ref

--- OpenApiTest/AnimalHouse.DesignFirst/Startup.cs
+++ OpenApiTest/AnimalHouse.DesignFirst/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnimalHouse.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +32,8 @@ namespace AnimalHouse.DesignFirst
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "AnimalHouse.DesignFirst", Version = "v1"});
             });
+
+            services.AddSingleton<IAnimalRepository, AnimalRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

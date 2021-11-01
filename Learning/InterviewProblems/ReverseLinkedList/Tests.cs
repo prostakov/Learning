c936ff9b78c3934817be033b30868f271452cb58
Serhii Prostakov <serhii.prostrakov@gmail.com>
2021-11-01 16:32:10 +0200

    Move everything to Learning folder

--- Learning/InterviewProblems/ReverseLinkedList/Tests.cs
+++ Learning/InterviewProblems/ReverseLinkedList/Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using Common;
 using FluentAssertions;
 using NUnit.Framework;
@@ -16,6 +17,23 @@ namespace InterviewProblems.ReverseLinkedList
             list.Reverse();
             
             list.ToString().Should().Be("element3,element2,element1");
+        }
+
+        [Test]
+        public void METHOD()
+        {
+            unsafe
+            {
+                var s1 = "Some 1";
+                var s2 = "Some 1";
+                var result = object.ReferenceEquals(s1, s2);
+            
+                TypedReference tr = __makeref(s1);
+                IntPtr ptr = **(IntPtr**) (&tr);
+                
+                TypedReference tr2 = __makeref(s2);
+                IntPtr ptr2 = **(IntPtr**) (&tr2);
+            }
         }
     }
 }

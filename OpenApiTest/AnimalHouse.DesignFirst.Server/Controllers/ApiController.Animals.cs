5ba2f337896b98afe10582e9a61b83b7b5b76ff1
Serhii Prostakov <serhii.prostakov@gmail.com>
2021-11-05 18:32:28 +0200

    Converter fix

--- OpenApiTest/AnimalHouse.DesignFirst.Server/Controllers/ApiController.Animals.cs
+++ OpenApiTest/AnimalHouse.DesignFirst.Server/Controllers/ApiController.Animals.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AnimalHouse.Common.Repositories;
+using AnimalHouse.DesignFirst.Server.Converters;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AnimalHouse.DesignFirst.Server.Controllers
 {
+    [Authorize]
     public partial class ApiController
     {
         private readonly IAnimalRepository _animalRepository;
         
         public override async Task<ICollection<Bird>> AnimalsAll()
         {
-            throw new NotImplementedException();
+            var animals = await _animalRepository.GetAll();
+
+            var dtos = animals.Select(AnimalsConverter.ToDto).ToList();
+
+            return (ICollection<Bird>) dtos.Where(x => x is Bird).ToArray();
         }
 
         public override Task<Bird> AnimalsPOST(Bird body)

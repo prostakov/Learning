72c5b90cd38586dc2edbb72e25c2e0d7761aa3cc
Serhii Prostakov <serhii.prostakov@gmail.com>
2021-11-22 15:59:28 +0200

    Fix attributes

--- OpenApiTest/AnimalHouse.CodeFirst.Server/SwaggerWebhookCallbacks/Subscription/Dto/VendorCallback.cs
+++ OpenApiTest/AnimalHouse.CodeFirst.Server/SwaggerWebhookCallbacks/Subscription/Dto/VendorCallback.cs
@@ -5,26 +5,37 @@ namespace AnimalHouse.CodeFirst.Server.SwaggerWebhookCallbacks.Subscription.Dto
 {
     public class VendorCallback
     {
+        [Required]
         public string Name { get; set; }
     
+        [Required]
         public string Email { get; set; }
     
+        [Required]
         public string Phone { get; set; }
     
+        [Required]
         public string StreetAddress { get; set; }
     
+        [Required]
         public string AddressCity { get; set; }
     
+        [Required]
         public string AddressState { get; set; }
     
+        [Required]
         public string AddressPostalCode { get; set; }
         
         /// <summary>
         /// Country code in alpha-2 format
         /// </summary>
         /// <example>US</example>
+        [Required]
         public string CountryCode { get; set; }
     
+        /// <summary>
+        /// Currency code
+        /// </summary>
         [Required]
         public string Currency { get; set; }
     

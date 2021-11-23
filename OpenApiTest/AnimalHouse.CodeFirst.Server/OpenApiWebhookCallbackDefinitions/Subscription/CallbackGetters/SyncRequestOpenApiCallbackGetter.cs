f7d5c455264b1633fb86c847436bb868a5771567
Serhii Prostakov <serhii.prostakov@gmail.com>
2021-11-23 15:21:19 +0200

    Complete SyncRequestOpenApiCallbackGetter

--- OpenApiTest/AnimalHouse.CodeFirst.Server/OpenApiWebhookCallbackDefinitions/Subscription/CallbackGetters/SyncRequestOpenApiCallbackGetter.cs
+++ OpenApiTest/AnimalHouse.CodeFirst.Server/OpenApiWebhookCallbackDefinitions/Subscription/CallbackGetters/SyncRequestOpenApiCallbackGetter.cs
@@ -1,14 +1,69 @@
 using System;
 using System.Collections.Generic;
+using AnimalHouse.CodeFirst.Server.OpenApiWebhookCallbackDefinitions.Subscription.Dto;
+using AnimalHouse.CodeFirst.Server.Responses;
+using AnimalHouse.CodeFirst.Server.Swagger.Extensions;
 using AnimalHouse.CodeFirst.Server.Swagger.WebhookCallbacks;
+using Microsoft.OpenApi.Expressions;
 using Microsoft.OpenApi.Models;
 
 namespace AnimalHouse.CodeFirst.Server.OpenApiWebhookCallbackDefinitions.Subscription.CallbackGetters
 {
     public class SyncRequestOpenApiCallbackGetter : SubscriptionCallbackGetterBase, IOpenApiCallbackGetter
     {
-        public const string Name = "syncRequest";
-        public OpenApiCallback Callback => new OpenApiCallback();
-        public HashSet<Type> Types => new HashSet<Type>();
+        public OpenApiCallback Callback => new()
+        {
+            PathItems = new Dictionary<RuntimeExpression, OpenApiPathItem>()
+            {
+                {
+                    RuntimeExpression.Build(BaseUrl + "syncRequest"),
+                    new OpenApiPathItem
+                    {
+                        Operations = new Dictionary<OperationType, OpenApiOperation>
+                        {
+                            {
+                                OperationType.Post, new OpenApiOperation
+                                {
+                                    Summary = "Callback initiated sync master data is triggered from VIC.AI admin panel.",
+                                    RequestBody = OpenApiExtensions.GetRequestBody<RequestIdCallback>(),
+                                    Description = Description,
+                                    Deprecated = false,
+                                    Responses = new OpenApiResponses
+                                    {
+                                        {"200", new OpenApiResponse {Description = "Request acknowleged"}},
+                                        {"default", OpenApiExtensions.GetResponseBody<ErrorApiResponse>("Request unservicable")},
+                                    },
+                                    Parameters = new List<OpenApiParameter> { RequestIdParameter }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        public HashSet<Type> Types => new()
+        {
+            typeof(RequestIdCallback),
+            typeof(ErrorApiResponse)
+        };
+
+        private string Description =>
+            "This request is sent when a user in Vic.ai triggers a sync of all masterdata." + LineBreak +
+
+            "In response to this message, you’re expected to post masterdata (vendors, GL accounts, dimensions) " +
+            "into Vic.ai using the respective routes. You may perform a partial update, determining which data " +
+            "to update using queries." + LineBreak +
+
+            "Once this process is completed, perform a syncUpdate patch, passing the syncId, which will allow Vic.ai " +
+            "to mark the user's sync action as completed with the 'complete' operation. If you cannot complete this " +
+            "process before the specified timeout, then perform a syncUpdate patch with the 'extend' operation, which " +
+            "will extend the timeout for the update. If the timeout expires before a 'complete' action has been sent, " +
+            "then the request will be marked as 'failed'." + LineBreak +
+
+            "The body of the syncRequest contains the requestId; this is a token that should be returned with all " +
+            "upsert actions associated with this syncRequest." + LineBreak +
+
+            "A 200 response indicates that the request has been acknowledged and informs the user that the sync has " +
+            "been successfully triggered.";
     }
 }
---
+++ OpenApiTest/AnimalHouse.CodeFirst.Server/OpenApiWebhookCallbackDefinitions/Subscription/Dto/RequestIdCallback.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AnimalHouse.CodeFirst.Server.OpenApiWebhookCallbackDefinitions.Subscription.Dto
+{
+    public class RequestIdCallback
+    {
+        public Guid ExternalId { get; set; }
+    }
+}

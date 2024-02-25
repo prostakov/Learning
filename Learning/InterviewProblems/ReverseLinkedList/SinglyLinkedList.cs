6e21d5cc8648b0159a4e810e55b673d793482ff7
Serhii Prostakov <serhii.prostakov@gmail.com>
2024-02-25 19:05:25 +0200

    Minor refactoring

--- Learning/InterviewProblems/ReverseLinkedList/SinglyLinkedList.cs
+++ Learning/InterviewProblems/ReverseLinkedList/SinglyLinkedList.cs
@@ -1,53 +1,51 @@
 using System.Text;
 
-namespace InterviewProblems.ReverseLinkedList
+namespace InterviewProblems.ReverseLinkedList;
+
+public class Node
 {
-    public class Node
+    public string Value { get; set; }
+    public Node Next { get; set; }
+}
+
+public class SinglyLinkedList
+{
+    private Node _head;
+
+    public SinglyLinkedList(params string[] items)
     {
-        public string Value { get; set; }
-        public Node Next { get; set; }
+        _head = null;
+        for (int i = items.Length-1; i >= 0; i--)
+        {
+            var current = new Node
+            {
+                Value = items[i], 
+                Next = _head
+            };
+            _head = current;
+        }
     }
 
-    public class SinglyLinkedList
+    public void Reverse()
     {
-        private Node _head;
+        Node previous = null;
+        Node current = _head;
+        while (current != null) 
+            (previous, current, current.Next) = (current, current.Next, previous);
+        _head = previous;
+    }
 
-        public SinglyLinkedList(params string[] items)
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        var current = _head;
+        while (current != null)
         {
-            _head = null;
-            for (int i = items.Length-1; i >= 0; i--)
-            {
-                var current = new Node
-                {
-                    Value = items[i], 
-                    Next = _head
-                };
-                _head = current;
-            }
+            sb.Append(current.Value);
+            sb.Append(",");
+            current = current.Next;
         }
-
-        public void Reverse()
-        {
-            // TODO: Ask interviewee to add implementation
-            // Node previous = null;
-            // Node current = _head;
-            // while (current != null) 
-            //     (previous, current, current.Next) = (current, current.Next, previous);
-            // _head = previous;
-        }
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            var current = _head;
-            while (current != null)
-            {
-                sb.Append(current.Value);
-                sb.Append(",");
-                current = current.Next;
-            }
-            sb.Length--;
-            return sb.ToString();
-        }
+        sb.Length--;
+        return sb.ToString();
     }
 }
--- Learning/InterviewProblems/ReverseLinkedList/Tests.cs
+++ Learning/InterviewProblems/ReverseLinkedList/Tests.cs
@@ -3,37 +3,36 @@ using Common;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace InterviewProblems.ReverseLinkedList
+namespace InterviewProblems.ReverseLinkedList;
+
+public class Tests : BaseTest
 {
-    public class Tests : BaseTest
+    [Test]
+    public void Test()
     {
-        [Test]
-        public void Test()
-        {
-            var list = new SinglyLinkedList("element1", "element2", "element3");
+        var list = new SinglyLinkedList("element1", "element2", "element3");
 
-            list.ToString().Should().Be("element1,element2,element3");
+        list.ToString().Should().Be("element1,element2,element3");
             
-            list.Reverse();
+        list.Reverse();
             
-            list.ToString().Should().Be("element3,element2,element1");
-        }
+        list.ToString().Should().Be("element3,element2,element1");
+    }
 
-        [Test]
-        public void METHOD()
+    [Test]
+    public void METHOD()
+    {
+        unsafe
         {
-            unsafe
-            {
-                var s1 = "Some 1";
-                var s2 = "Some 1";
-                var result = object.ReferenceEquals(s1, s2);
+            var s1 = "Some 1";
+            var s2 = "Some 1";
+            var result = object.ReferenceEquals(s1, s2);
             
-                TypedReference tr = __makeref(s1);
-                IntPtr ptr = **(IntPtr**) (&tr);
+            TypedReference tr = __makeref(s1);
+            IntPtr ptr = **(IntPtr**) (&tr);
                 
-                TypedReference tr2 = __makeref(s2);
-                IntPtr ptr2 = **(IntPtr**) (&tr2);
-            }
+            TypedReference tr2 = __makeref(s2);
+            IntPtr ptr2 = **(IntPtr**) (&tr2);
         }
     }
 }

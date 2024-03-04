c2936babe6c6133cd20220a32010438f8d6fe230
Serhii Prostakov <serhii.prostakov@gmail.com>
2024-03-04 03:36:18 +0200

    Add GraphDiameter problem

---
+++ Learning/InterviewProblems/Graph/GraphDiameter/Graph.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+
+namespace InterviewProblems.Graph.GraphDiameter;
+
+public class Node(int value)
+{
+    public int Value { get; set; } = value;
+    public List<Node> Edges { get; set; } = new();
+}
+
+public class Graph
+{
+    public List<Node> Nodes { get; }
+
+    public Graph()
+    {
+        Nodes = [];
+    }
+    
+    public Node AddNode(int value)
+    {
+        var node = new Node(value);
+        Nodes.Add(node);
+        return node;
+    }
+
+    public void AddEdge(Node node1, Node node2)
+    {
+        node1.Edges.Add(node2);
+        node2.Edges.Add(node1);
+    }
+
+    public (Node, int) BFS(Node startNode)
+    {
+        var visited = new Dictionary<Node, bool>();
+        var distances = new Dictionary<Node, int>();
+        var queue = new Queue<Node>();
+
+        foreach (var node in Nodes)
+        {
+            visited[node] = false;
+            distances[node] = int.MaxValue;
+        }
+
+        visited[startNode] = true;
+        distances[startNode] = 0;
+        queue.Enqueue(startNode);
+
+        Node furthestNode = startNode;
+        int maxDistance = 0;
+
+        while (queue.Count != 0)
+        {
+            var node = queue.Dequeue();
+
+            foreach (var neighbor in node.Edges)
+            {
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    distances[neighbor] = distances[node] + 1;
+                    queue.Enqueue(neighbor);
+
+                    if (distances[neighbor] > maxDistance)
+                    {
+                        maxDistance = distances[neighbor];
+                        furthestNode = neighbor;
+                    }
+                }
+            }
+        }
+
+        return (furthestNode, maxDistance);
+    }
+}
---
+++ Learning/InterviewProblems/Graph/GraphDiameter/Tests.cs
@@ -0,0 +1,40 @@
+using Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace InterviewProblems.Graph.GraphDiameter;
+
+public class Tests : BaseTest
+{
+    [Test]
+    public void Test()
+    {
+        var graph = new Graph();
+
+        var node0 = graph.AddNode(0);
+        var node1 = graph.AddNode(1);
+        var node2 = graph.AddNode(2);
+        var node3 = graph.AddNode(3);
+        var node4 = graph.AddNode(4);
+        var node5 = graph.AddNode(5);
+        var node6 = graph.AddNode(6);
+        var node7 = graph.AddNode(7);
+        var node8 = graph.AddNode(8);
+        var node9 = graph.AddNode(9);
+        
+        graph.AddEdge(node0, node1);
+        graph.AddEdge(node1, node2); 
+        graph.AddEdge(node2, node3); 
+        graph.AddEdge(node2, node9); 
+        graph.AddEdge(node2, node4); 
+        graph.AddEdge(node4, node5); 
+        graph.AddEdge(node1, node6); 
+        graph.AddEdge(node6, node7); 
+        graph.AddEdge(node6, node8); 
+        
+        var (furthestNode, _) = graph.BFS(node5);
+        var (_, longestPath) = graph.BFS(furthestNode);
+
+        longestPath.Should().Be(5);
+    }
+}
